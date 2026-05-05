2d5ffd103cc58fe185d8e9f2cb988da5858249be
Isimbi Jade Gladys <gladysjade9@gmail.com>
2026-05-05 18:10:21 +0200

    JMS-21: implement JWT authentication middleware for protected routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const initializeDatabase = require("./schema/init");
-const authRoutes = require("./routes/authRoutes")
+const authRoutes = require("./routes/authRoutes");
+const authenticateToken = require("./middleware/authMiddleware");
 
 const app = express();
 const PORT = process.env.PORT || 5050;
@@ -22,3 +23,11 @@ app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/api/auth/me", authenticateToken, (req, res) => {
+    res.status(200).json({
+        message: "Authenticated user fetched successfully.",
+        user: req.user,
+    });
+});
+
