2176007db592210d1624c94284ea49be9c24c037
Isimbi Jade Gladys <gladysjade9@gmail.com>
2026-05-05 13:42:31 +0200

    JMS-17: set up environment variables and add backend env example

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
-const dbPath = path.join(__dirname, "../../job-mng-system.sqlite");
+const dbPath = path.resolve(__dirname, "../../", process.env.DATABASE_URL || "./job-mng-system.sqlite");
 
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
