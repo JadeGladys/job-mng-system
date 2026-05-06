25f43ad404f9215852506e5219f030b9de4d1e10
Isimbi Jade Gladys <gladysjade9@gmail.com>
2026-05-06 10:50:47 +0200

    JMS-22: implement role-based authorization middleware for protected routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const initializeDatabase = require("./schema/init");
 const authRoutes = require("./routes/authRoutes");
 const authenticateToken = require("./middleware/authMiddleware");
+const authorizeRoles = require("./middleware/authorizeRoles");
 
 const app = express();
 const PORT = process.env.PORT || 5050;
@@ -20,14 +21,25 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
+//temporary routes for testing
+//Authentication
 app.get("/api/auth/me", authenticateToken, (req, res) => {
     res.status(200).json({
         message: "Authenticated user fetched successfully.",
         user: req.user,
     });
 });
+
+//Authorization
+app.get("/api/admin/test", authenticateToken, authorizeRoles("admin"), (req, res) => {
+    res.status(200).json({
+        message: "Admin access granted.",
+        user: req.user,
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 
