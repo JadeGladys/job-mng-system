ff2be5534bfb13472e5ce28bc6e174123c52d445
Isimbi Jade Gladys <gladysjade9@gmail.com>
2026-05-06 12:21:28 +0200

    JMS-24: build login and registration pages with shared auth layout

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,15 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const initializeDatabase = require("./schema/init");
 const authRoutes = require("./routes/authRoutes");
-const authenticateToken = require("./middleware/authMiddleware");
 const authorizeRoles = require("./middleware/authorizeRoles");
 
 const app = express();
 const PORT = process.env.PORT || 5050;
 
+app.use(cors());
 app.use(express.json());
 
 initializeDatabase();
@@ -21,22 +22,14 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 
-//temporary routes for testing
-//Authentication
-app.get("/api/auth/me", authenticateToken, (req, res) => {
-    res.status(200).json({
-        message: "Authenticated user fetched successfully.",
-        user: req.user,
-    });
-});
-
+/*temporary admin route for testing
 //Authorization
 app.get("/api/admin/test", authenticateToken, authorizeRoles("admin"), (req, res) => {
     res.status(200).json({
         message: "Admin access granted.",
         user: req.user,
     });
-});
+});*/
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
